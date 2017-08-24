691828e7c052ea8ea7ac8eb8eac48be7e7902b80
Alexander <mr.savkin.alexander@yandex.ru>
2017-08-24 21:31:21 +0300

    Final

--- src/app.js
+++ src/app.js
@@ -1,43 +1,71 @@
-var max = 19;
-var min = 1;
-var firstTerm = Math.floor(Math.random() * (max - min + 1)) + min;
-var secondTerm = Math.floor(Math.random() * (20 - firstTerm - min + 1)) + min;
+// set terms
+
+var firstTermRange = {
+	min: 6,
+	max: 9
+};
+
+var summRange = {
+	min: 11,
+	max: 14
+};
+
+var firstTerm = Math.floor(Math.random() * (firstTermRange.max - firstTermRange.min + 1)) + firstTermRange.min;
+
+var secondTermRange = {
+	min: summRange.min - firstTerm,
+	max: summRange.max - firstTerm
+};
+
+var secondTerm = Math.floor(Math.random() * (secondTermRange.max - secondTermRange.min + 1)) + secondTermRange.min;
+
 var summ = firstTerm + secondTerm;
 var start = 0; 
 
-var svg = document.getElementById('svg');
-var firstTermNode = document.getElementById('1');
-var secondTermNode = document.getElementById('2');
 
-var input1 = document.getElementById('input1');
-var input2 = document.getElementById('input2');
-var input3 = document.getElementById('input3');
+// save link to dom elements
+var dom = {
+	svg: document.getElementById('svg'),
+	firstTerm: document.getElementById('1'),
+	secondTerm: document.getElementById('2'),
+	answer: document.getElementById('3'),
+	input1: document.getElementById('input1'),
+	input2: document.getElementById('input2'),
+	input3: document.getElementById('input3'),
+	inputsWrap: document.getElementById('inputs-wrap'),
+	bodyContent: document.getElementById('content')
+};
 
-input1.addEventListener('change', onInputChange1);
-input2.addEventListener('change', onInputChange2);
-input3.addEventListener('change', onInputChange3);
+// add event listeners
+dom.input1.addEventListener('change', onInputChange1);
+dom.input2.addEventListener('change', onInputChange2);
+dom.input3.addEventListener('change', onInputChange3);
 
-firstTermNode.appendChild(document.createTextNode(firstTerm));
-secondTermNode.appendChild(document.createTextNode(secondTerm));
+// render terms
+dom.firstTerm.appendChild(document.createTextNode(firstTerm));
+dom.secondTerm.appendChild(document.createTextNode(secondTerm));
 
+// 39px is equal one segment
 var step = 39;
 
-function renderCircle(cx, cy, rx) {
-	var circle = document.createElementNS("http://www.w3.org/2000/svg", 'ellipse');
+// render svg ellipse
+function renderEllipse(cx, cy, rx) {
+	var ellipse = document.createElementNS("http://www.w3.org/2000/svg", 'ellipse');
 
-	circle.setAttribute('cx', cx);
-	circle.setAttribute('cy', cy);
-	circle.setAttribute('rx', rx);
-	circle.setAttribute('ry', 100);
+	ellipse.setAttribute('cx', cx);
+	ellipse.setAttribute('cy', cy);
+	ellipse.setAttribute('rx', rx);
+	ellipse.setAttribute('ry', 50);
 
-	circle.setAttribute('stroke', "rgb(143,23,23)");
-	circle.setAttribute('stroke-width', 2);
-	circle.setAttribute('fill', "green");
-	circle.setAttribute('fill-opacity', 0);
+	ellipse.setAttribute('stroke', "rgb(143,23,23)");
+	ellipse.setAttribute('stroke-width', 2);
+	ellipse.setAttribute('fill', "green");
+	ellipse.setAttribute('fill-opacity', 0);
 
-	svg.appendChild(circle);
+	dom.svg.appendChild(ellipse);
 }
 
+// renser svg arrow
 function renderArrow(firstTerm, secondTerm) {
 	var middle = (firstTerm + secondTerm) / 2;
 
@@ -46,33 +74,59 @@ function renderArrow(firstTerm, secondTerm) {
 	var cx = center;
 	var r = ((secondTerm - firstTerm) / 2) * step;
 
-	renderCircle(cx, cy, r);
+	dom.inputsWrap.style.left = cx + 173;
+
+	renderEllipse(cx, cy, r);
 }
 
 function onInputChange1(event) {
-	if (+input1.value === firstTerm) {
-		input1.style.display = 'none';
+	if (+dom.input1.value === firstTerm) {
+		dom.input1.style.display = 'none';
 		input2.style.display = 'inline-block';
+		dom.firstTerm.style.backgroundColor = 'white';
+
+		var bounds = dom.inputsWrap.getBoundingClientRect();
+		var answerNode = document.createElement('div');
+		answerNode.innerHTML = firstTerm;
+		answerNode.style.position = 'absolute';
+		answerNode.style.left = bounds.left - 85;
+		answerNode.style.bottom = 110;
+		dom.bodyContent.appendChild(answerNode);
+
 		renderArrow(firstTerm, summ);
 	} else {
-		input1.style.color = 'red';
+		dom.input1.style.color = 'red';
+		dom.firstTerm.style.backgroundColor = 'yellow';
 	}
 }
 
 function onInputChange2(event) {
-	if (+input2.value === secondTerm) {
-		input2.style.display = 'none';
+	if (+dom.input2.value === secondTerm) {
+		dom.input2.style.display = 'none';
 		input3.style.display = 'inline-block';
+		dom.secondTerm.style.backgroundColor = 'white';
+
+		var bounds = dom.inputsWrap.getBoundingClientRect();
+		var answerNode = document.createElement('div');
+		answerNode.innerHTML = secondTerm;
+		answerNode.style.position = 'absolute';
+		answerNode.style.left = bounds.left - 80;
+		answerNode.style.bottom = 110;
+		dom.bodyContent.appendChild(answerNode);
+
+		dom.answer.innerHTML = '';
 	} else {
-		input2.style.color = 'red';
+		dom.input2.style.color = 'red';
+		dom.secondTerm.style.backgroundColor = 'yellow';
 	}
 }
 
 function onInputChange3(event) {
-	if (+input3.value === summ) {
-		input3.style.display = 'none';
+	if (+dom.input3.value === summ) {
+		dom.input3.style.display = 'none';
+		dom.answer.innerHTML = summ;
 	} else {
-		input3.style.color = 'red';
+		dom.input3.style.color = 'red';
 	}
 }
 
